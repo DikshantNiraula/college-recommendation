86acc686c1f685b401c87ea65290cd06b0989bf6
dikshantniraula <dikshantniraula351@gmail.com>
2023-05-14 08:22:23 +0545

    ROund off distance

--- resources/views/ui/travel-college.blade.php
+++ resources/views/ui/travel-college.blade.php
@@ -50,33 +50,7 @@
             </div>
      
             </div>
-            <div class="row">
-               <div class="col-md-12">
-                  <ul class="pagination lms-page lms-pagination">
-                     <li class="page-item prev">
-                        <a class="page-link" href="javascript:void(0);" tabindex="-1"><i class="fas fa-angle-left"></i></a>
-                     </li>
-                     <li class="page-item first-page active">
-                        <a class="page-link" href="javascript:void(0);">1</a>
-                     </li>
-                     <li class="page-item">
-                        <a class="page-link" href="javascript:void(0);">2</a>
-                     </li>
-                     <li class="page-item">
-                        <a class="page-link" href="javascript:void(0);">3</a>
-                     </li>
-                     <li class="page-item">
-                        <a class="page-link" href="javascript:void(0);">4</a>
-                     </li>
-                     <li class="page-item">
-                        <a class="page-link" href="javascript:void(0);">5</a>
-                     </li>
-                     <li class="page-item next">
-                        <a class="page-link" href="javascript:void(0);"><i class="fas fa-angle-right"></i></a>
-                     </li>
-                  </ul>
-               </div>
-            </div>
+          
          </div>
          <div class="col-lg-3">
             <div class="filter-clear">
